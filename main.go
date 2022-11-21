5100d9ed82bad12ba78b4c7f7dc96463498effae
axel.karamoko <axel.karamoko@inexa-ci.com>
2022-11-21 01:16:40 +0000

    ...

--- main.go
+++ main.go
@@ -7,18 +7,30 @@ import (
 	"strings"
 )
 
+
+func determineQuad(quadName ){
+		
+}
+
 func main() {
 
+	
 	files, err := ioutil.ReadDir("./")
-
+	
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
+	// totalFiles := len(files)
+	
+	var quads [32]string
+
+	for i, file := range files {
 		name := file.Name()
-		if strings.Contains(name, "quad") {
+		if strings.Contains(name, "quad") && name != "quadchecker" {
+			quads[]
 			fmt.Println(name)
 		}
 	}
+
 }
